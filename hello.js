4e080a277f016c3055b1d7abe4e05b9728ce3866
boulosQ24-meet <boulosbqurbashi@gmail.com>
2023-07-17 11:34:42 +0300

    frontend

---
+++ hello.js
@@ -0,0 +1,8 @@
+function changeColor(){
+    if (document.body.style.backgroundColor == "yellowgreen"){
+        document.body.style.backgroundColor="black";
+    }
+    else{
+        document.body.style.backgroundColor="yellowgreen";
+    }
+}
